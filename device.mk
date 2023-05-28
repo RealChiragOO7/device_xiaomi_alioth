ece5cc3a996d75d46bcfdf277a170c1a12d9a499
RealChiragOO7 <chirag1044gupta@gmail.com>
2023-05-28 18:36:12 +0530

    Revert: Enable Dolby Support and Import needed libs

--- device.mk
+++ device.mk
@@ -26,9 +26,6 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/st_license.lic:$(TARGET_COPY_OUT_VENDOR)/etc/camera/st_license.lic
 
-# Dolby Support
-TARGET_USES_MIUI_DOLBY := true
-
 # Dolby Config File
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/dolby/config/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
